90e5e99ff88b5160db011dbf77aae28691db77b8
Sasha Lawrence <sashalawrence@outlook.com>
2019-09-28 18:33:17 +0100

    Updates

---
+++ precache-manifest.0abcc91b81df0deea44c51d358edda28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efc4692ca02b7710f419d749033a70c",
+    "url": "/pingpong-app/index.html"
+  },
+  {
+    "revision": "be2081ebd2d928574c84",
+    "url": "/pingpong-app/static/css/main.d856f3b6.chunk.css"
+  },
+  {
+    "revision": "80cc6a9c1b3e494c8f43",
+    "url": "/pingpong-app/static/js/2.83096f9d.chunk.js"
+  },
+  {
+    "revision": "be2081ebd2d928574c84",
+    "url": "/pingpong-app/static/js/main.d994cdb6.chunk.js"
+  },
+  {
+    "revision": "7e4f211683b691967b40",
+    "url": "/pingpong-app/static/js/runtime-main.40b007d3.js"
+  },
+  {
+    "revision": "cb9686b258bd166b5b7d95eac187ae65",
+    "url": "/pingpong-app/static/media/logo.cb9686b2.png"
+  }
+]);
